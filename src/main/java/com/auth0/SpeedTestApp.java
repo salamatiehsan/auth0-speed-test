55608f5e8cc299ca2551db1dda6ce8f69cb0b67f
Ehsan Salamati <ehsan@ritual.co>
2019-06-18 21:48:43 -0400

    This is the first commit

--- src/main/java/com/auth0/SpeedTestApp.java
+++ src/main/java/com/auth0/SpeedTestApp.java
@@ -22,7 +22,7 @@ public class SpeedTestApp {
     }
 
     public static void main(String[] args) throws InvalidProtocolBufferException {
-        //comment added by b1
+        //comment added by master
         final List<Person> peopleList = new ArrayList<>();
         for (int i = 0; i < 50000; i++) {
             final Address address1 = Address.newBuilder()
